f0ab60fe06b28d928bb0fdd0ce2fc08177717f74
tiapezz <mattia.pezzolla.mobile@gmail.com>
2022-11-11 15:25:17 +0100

    k

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,15 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
   "/precache-manifest.33847bcc7d4fc08145c516c6fc2828ee.js"
-=======
-<<<<<<< HEAD
-  "/precache-manifest.ed9c20d10bf7435a49c60ea717c036f4.js"
-=======
-  "/precache-manifest.70a3671102b18a1bd91d817dafc463c6.js"
->>>>>>> parent of 08989c6c (Revert "cose 2")
->>>>>>> parent of 8e44b72c (max)
 );
 
 workbox.clientsClaim();
