a8ad108209e596cd78b03796501b4d6abe3c236e
tiapezz <mattia.pezzolla.mobile@gmail.com>
2022-11-11 15:08:25 +0100

    k

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33847bcc7d4fc08145c516c6fc2828ee.js"
+  "/precache-manifest.d9a028b813c6fd34734476123e68544c.js"
 );
 
 workbox.clientsClaim();
